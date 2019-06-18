5bfc9024dc430955e3ee4ff1ba2d3edf8547d29b
auyeskhanm <auyeskhanm@gmail.com>
2019-06-18 19:23:50 +0600

    Добавьте файлы проекта.

---
+++ 17.06.2019SP/Body.cs
@@ -0,0 +1,10 @@
+﻿namespace _17._06._2019SP
+{
+    internal class Body
+    {
+        public string firstName { get; set; }
+        public string secondName { get; set; }
+        public string percentage { get; set; }
+        public string result { get; set; }
+    }
+}
---
+++ 17.06.2019SP/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _17._06._2019SP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void CheckingButtonClick(object sender, RoutedEventArgs e)
+        {
+            var example = await LoadBody(firstNameTextBox.Text, secondNameTextBox.Text);
+
+            percentageTextBlock.Text = example.percentage + "%";
+            resultTextBlock.Text = example.result;
+        }
+
+        private Task<Body> LoadBody(string firstName, string secondName)
+        {
+            return Task.Run(() =>
+            {
+                WebClient client = new WebClient();
+                client.Headers.Add("X-RapidAPI-Host", "love-calculator.p.rapidapi.com");
+                client.Headers.Add("X-RapidAPI-Key", "3ef32f4b0cmsh9e73eff5f258946p1a3a7ajsn94dc434d6f67");
+
+                string json = client.DownloadString("https://love-calculator.p.rapidapi.com/getPercentage?fname=" + firstName + "&sname=" + secondName);
+
+                return JsonConvert.DeserializeObject<Body>(json);
+            });
+        }
+    }
+}
